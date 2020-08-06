f4c1836b50664f673a9674229474c4ad04bef3cc
Gowtham Uppuluri <ugowthamsai@gmail.com>
2020-08-06 13:27:17 +0530

    Updates

---
+++ precache-manifest.3a9ce4fd6665d83f6cd8b020f2952389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45a18e582e46e03f32627e530487a9a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b02d20910bbfa7ef31aa",
+    "url": "/contactmanager/static/css/2.0ac40688.chunk.css"
+  },
+  {
+    "revision": "003c877cc27bee10b7e7",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b02d20910bbfa7ef31aa",
+    "url": "/contactmanager/static/js/2.d80c0688.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.d80c0688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003c877cc27bee10b7e7",
+    "url": "/contactmanager/static/js/main.cd8761cb.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
