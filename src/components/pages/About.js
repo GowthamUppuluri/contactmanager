1db1a8eb4c872ab6321bcc19aecf13b7880659db
Gowtham Uppuluri <ugowthamsai@gmail.com>
2020-08-06 18:06:59 +0530

    Seventh Push

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Admin from "../images/Title.jpg";
 export default function About() {
   return (
     <div>
-      <h1 className="display-4">About Contact Manager</h1>
-      <p className="lead">Simple App to manage contacts</p>
-      <p className="text-secondary">Version</p>
+      <center>
+        <h1 className="display-4">About Contact Manager</h1>
+        <img src={Admin} />
+        <p className="lead">Simple App to manage contacts</p>
+        <p className="text-secondary">Version 1.0.0</p>
+      </center>
     </div>
   );
 }
