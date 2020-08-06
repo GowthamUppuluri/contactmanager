52145d77b8087203143e87088179751a29a0a134
Gowtham Uppuluri <ugowthamsai@gmail.com>
2020-08-06 17:39:29 +0530

    Third deploy

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,9 +8,9 @@ const Header = ({ branding }) => {
      navbar-dark bg-danger mb-3 py-0"
     >
       <div className="container">
-        <Link href="/" className="navbar-brand">
+        <a href="/#/" className="navbar-brand">
           {branding}
-        </Link>
+        </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
